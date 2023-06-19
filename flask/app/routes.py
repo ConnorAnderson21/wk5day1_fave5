3edf92543e87d18b02e04d11c80102f7480ea889
ConnorAnderson21 <connoranderson21@gmail.com>
2023-06-18 20:25:29 -0400

    jinja ninja

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -7,8 +7,35 @@ def land():
     return render_template('index.html')
 
 @app.route('/favorite5')
-def home():
-    return render_template('favorite5.html')
+def favorite5():
+    athletes = [
+        {
+            'name':'Tom Brady',
+            'sport':'Football',
+            'position':'Quarterback'
+        },
+        {
+            'name':'Rob Gronkowski',
+            'sport':'Football',
+            'position':'Tight End'
+        },
+        {
+            'name':'David Ortiz',
+            'sport':'Baseball',
+            'position':'Designated Hitter'
+        },
+        {
+            'name':'Kevin Garnett',
+            'sport':'Basketball',
+            'position':'Power Forward'
+        },
+        {
+            'name':'Manny Ramirez',
+            'sport':'Baseball',
+            'position':'Outfield'
+        }
+    ]
+    return render_template('favorite5.html', fave5=athletes)
 
 
     
